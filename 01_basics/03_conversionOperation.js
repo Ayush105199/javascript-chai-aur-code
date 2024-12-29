692dd244d52ddc1405915f7123f1537910179f4f
Ayush105199 <119776681+Ayush105199@users.noreply.github.com>
2024-12-29 09:07:04 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score ="33";
+// console.log(typeof score);
+let value=Number(score);
+// console.log(typeof value);
+// console.log(value);
+
+let b="hitesh";
+let bool1=Boolean(b);
+// console.log(bool1);
+//1-> true;
+//0->false;
+//""=>false;
+//"hitesh"=>true;
+let c=33
+
+let str=String(c);
+console.log(str);
+console.log(typeof str);
+
