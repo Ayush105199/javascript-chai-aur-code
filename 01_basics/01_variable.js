826b6125240bf098295b03703ea2c570893db8dc
Ayush105199 <119776681+Ayush105199@users.noreply.github.com>
2024-12-30 16:06:07 +0530

    array techniques

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -2,10 +2,9 @@ const accountId =144553
 let accountEmail= "hitesh@google.com"
 var accountPassword ="12345"
 accountCity ="jaipur"
-<<<<<<< HEAD
+
 accountId=2
 console.log(accountId);
-=======
 accountEmail="hc@hc.com"
 accountPassword="212121212"
 accountCity="Bengaluru"
@@ -16,15 +15,12 @@ let accountState;
 // console.log(accountCity);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 /* Prefer not to use var because of issue in block scope and functional scope*/
->>>>>>> 692dd244d52ddc1405915f7123f1537910179f4f
-
-<<<<<<< HEAD
-
-
-=======
 
 
 
 
 
->>>>>>> 692dd244d52ddc1405915f7123f1537910179f4f
+
+
+
+
